dbabfb2a0714a924df6b17196d8bf5873c1180ec
Ops <krzysztof.papciak@hotmail.com>
2022-09-07 20:18:51 +0200

    Add customer and vehicle entities

---
+++ src/CarManager.Domain/Entities/Customer.cs
@@ -0,0 +1,17 @@
+﻿namespace CarManager.Domain.Entities;
+
+//TODO implement Customer
+public class Customer : Entity<CustomerId>
+{
+}
+
+public record CustomerId : Id
+{
+    public CustomerId() : base()
+    {
+    }
+
+    public CustomerId(Guid id) : base(id)
+    {
+    }
+}
---
+++ src/CarManager.Domain/Entities/Vehicle.cs
@@ -0,0 +1,17 @@
+﻿namespace CarManager.Domain.Entities;
+
+//TODO implement Vehicle
+public class Vehicle : Entity<VehicleId>
+{
+}
+
+public record VehicleId : Id
+{
+    public VehicleId() : base()
+    {
+    }
+
+    public VehicleId(Guid id) : base(id)
+    {
+    }
+}
